6c9d12582de22b0122ee9e37fa855e9a776b4353
Andrew Musgrave <andrew.musgrave@shopify.com>
2018-04-15 12:56:17 -0400

    Removed await

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = ({boundActionCreators, graphql}) => {
     const posts = res.data.allMarkdownRemark.edges;
 
     posts.forEach(async ({node}) => {
-      await axios.post(`https://programming-paradigms-api.herokuapp.com/api/post/`, {
+      axios.post(`https://programming-paradigms-api.herokuapp.com/api/post/`, {
           title: node.frontmatter.title,
           identifier: node.frontmatter.identifier,
       }).catch(err => console.log(`Unable to create post comment section: ${err}`));
