39449df789dcd3357b685e0aa13a38d82d42c2ca
Andrew Musgrave <andrewdmusgrave@gmail.com>
2018-04-28 23:35:40 -0400

    Added new url

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,8 @@ function IndexPage ({data}) {
           <img
             alt="author"
             style={{minHeight: '200px', minWidth: '200px'}}
-            src="https://farm5.staticflickr.com/4438/36177469644_0c7cf43939_o.jpg"
+            // src="https://farm5.staticflickr.com/4438/36177469644_0c7cf43939_o.jpg"
+            src="https://farm1.staticflickr.com/979/40870772275_ccd9f3d7c4_n.jpg"
             width="200"
             height="200"
           />
